f09295a8159db42607455534a1033538c88bd4fc
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-03-13 12:20:10 +0000

    Updated to run as a loop for continuous translation

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main(text_input):
     Main function for running translations.
     :return:
     """
-    integrate_chatgpt_test.set_context("you are a sim from the game, 'the sims' - whatever i type in in english, "
+    integrate_chatgpt_test.set_context("you are a sim from the game, 'the sims' - whatever i type in, "
                                        "please translate to simlish")
 
     integrate_chatgpt_test.set_text_input(text_input)
@@ -24,4 +24,6 @@ def main(text_input):
 
 
 if __name__ == "__main__":
-    print(main("hello how are you today, would you like to go into old town"))
+    while True:
+        text_input = input("Enter text to translate to Simlish: ")
+        print(main(text_input))
